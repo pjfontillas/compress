8876ecbbbb543c63f93f4c206c0f1a2d7166f386
Patrick James Fontillas <me@pjfontillas.com>
2013-10-20 00:54:54 -0400

    Removed error param.

--- www/create_db.php
+++ www/create_db.php
@@ -12,13 +12,13 @@ try {
 // add links table to database
 $query = 'CREATE TABLE links ' .
         '(full_url TEXT, short_url TEXT);';
-$database->exec($query, $error);
+$database->exec($query);
 echo '<p>Successfully created \'links\' table.</p>';
 
 // add redirect table to database
 $query = 'CREATE TABLE redirects' .
         '(short_url TEXT, ip TEXT, proxy TEXT, time TEXT);';
-$database->exec($query, $error);
+$database->exec($query);
 echo '<p>Successfully created \'redirects\' table.</p>';
 
 unset($database);
